3657dbef6fd73690f68baa315ccc083aa51d95f0
RaeKimie <nr.kim91@gmail.com>
2020-02-06 20:09:51 +0000

    day 1 java script strings

---
+++ strings.js
@@ -0,0 +1,13 @@
+let firstName = "Jabez";
+let lastName = "souttar";
+let fullName = firstName + " " + lastName;
+// console.log(firstName, "<--firstName");
+// console.log(lastName, "<--lastName");
+// console.log(fullName, "<--fullName");
+
+let city = "York";
+let country = "England";
+let location = city + " " + country;
+// console.log(city, "<--city");
+// console.log(country, "<--country");
+// console.log(location, "<--location");
