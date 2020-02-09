b06a0110d95c34ebd5c7d66e83c4e4f94729dbb9
Bezll <jabez1992@hotmail.com>
2020-02-09 21:55:00 +0000

    booleans

---
+++ boolean.js
@@ -0,0 +1,15 @@
+// === equality operator
+// !== not equal operator
+// < / >  less / more than operator
+// <= / >= less / more than or equal to
+
+
+let age = 6
+
+if (age <= 7) {
+    console.log('Entrance is free of charge')
+}
+
+if (age >= 65) {
+    console.log('Tickets are £12')
+}
---
+++ statements.js
@@ -0,0 +1,9 @@
+let temp = 40
+
+ if (temp <= 0) {
+     console.log('It is Freezing Outside!')
+}
+
+if (temp >= 35) {
+    console.log('it is Boiling!')
+}
