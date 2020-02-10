cb324e3851d3b0a2ebb4577750755baecaf5d7d3
Bezll <jabez1992@hotmail.com>
2020-02-10 21:21:42 +0000

    booleans-advanced.js

---
+++ boolean-advanced.js
@@ -0,0 +1,12 @@
+let temp = 5
+let isFreezing = temp <= 0
+let isBoiling = temp >= 40
+
+
+if (isFreezing) {
+    console.log('Cold')
+} else if (isBoiling) {
+    console.log('Hot')
+} else [
+    console.log('Nice')
+]
