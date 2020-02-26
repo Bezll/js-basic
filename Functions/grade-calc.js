f5e6d3e9c72a3bc3af7e99e4fa614a536587ef46
Bezll <jabez1992@hotmail.com>
2020-02-26 21:10:35 +0000

    End of Section 4

---
+++ Functions/grade-calc.js
@@ -0,0 +1,31 @@
+// Student score, total possible score
+// 15/20 -> you got a C (75%)!
+// A 90-100, B 80-89, C 70-79 D 60-69 E 50 -59
+
+let gradeCalculator = function (studentScore, totalPosScore) {
+    let gradePercent = (studentScore / totalPosScore) * 100
+    let gradeScoring = 'No Score'
+    
+    if (gradePercent >= 90) {
+        gradeScoring = 'A'
+    }
+    else if (gradePercent >= 80) {
+        gradeScoring = 'B'
+    }
+    else if (gradePercent >= 70) {
+        gradeScoring = 'C'
+    }
+    else if (gradePercent >= 60) {
+        gradeScoring = 'D'
+    }
+    else if (gradePercent >= 50) {
+        gradeScoring = 'E'
+    }
+    else  if (gradePercent >= 1) {                  
+        gradeScoring = 'F'
+    }
+    return `You got a ${gradeScoring} (${gradePercent}%)!`;
+}
+
+let studentScoreAndTotalPosScore = gradeCalculator(0, 127)
+console.log(studentScoreAndTotalPosScore)
