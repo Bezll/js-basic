2ccf1156c4fd679397cb73edfb7dc7b72937291a
Bezll <jabez1992@hotmail.com>
2020-03-05 21:22:56 +0000

    Arrays

---
+++ Arrays/todo.js
@@ -0,0 +1,5 @@
+const naraesToDoList = ['Go shopping', 'Hoover', 'Make salad', 'Hang washing', 'Clean bathroom']
+
+console.log(`You have ${naraesToDoList.length} todos!`)
+console.log(`Todo: ${naraesToDoList[0]}`)
+console.log(`Todo: ${naraesToDoList[naraesToDoList.length - 2]}`)
